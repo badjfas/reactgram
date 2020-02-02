20f74e54c33a81f51ab5c822360a48757e8af91e
baejinnwoo <bjwkor@naver.com>
2020-02-02 17:57:59 +0900

    seeProfile,editUser

--- src/server.js
+++ src/server.js
@@ -4,13 +4,14 @@ import {prisma} from "../generated/prisma-client";
 import logger from "morgan";
 import schema from "./schema";
 import {authenticateJwt} from "./passport";
+import { isAuthenticated } from "./middlewares";
 
 const PORT = process.env.PORT || 4000;
 
 
 const server = new GraphQLServer({
     schema,
-    context: ({ request }) => ({ request})
+    context: ({ request }) => ({ request,isAuthenticated})
   });
   
   server.express.use(logger("dev"));
